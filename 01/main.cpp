e0322f78f7e139431fc62dcdb23a0ddea0843f4f
kolmX <47042803+kolmX@users.noreply.github.com>
2019-03-17 23:21:12 +0300

    Add files via upload

--- 01/main.cpp
+++ 01/main.cpp
@@ -20,8 +20,7 @@ int is_simple(const int n)
 //---------------------------bin_search_functions------------------------------
 
 //find fisrt position left element which is equal val
-int find_first(const int * arr,const int start,\
-								const int end,const int val)
+int find_first(const int * arr,const int start,const int end,const int val)
 {
 
 
@@ -44,21 +43,20 @@ int find_first(const int * arr,const int start,\
 
 
 //find last position rigth elemnt which is equal val
-int find_last(const int * arr,const int start,\
-								const int end,const int val)
+int find_last(const int * arr,const int start, const int end, const int val)
 {
 
 
-		if ((start==end-1)&&(arr[start]!=val))
+		if ((start == end-1) && (arr[start] != val))
 				return -1;
 
-		if (arr[(start+end)/2]>val)
+		if (arr[(start+end)/2] > val)
 				return find_last(arr,start,(start+end)/2,val);
 
-		if (arr[(start+end)/2]<val)
+		if (arr[(start+end)/2] < val)
 				return find_last(arr,(start+end)/2,end,val);
 
-		if (arr[(start+end)/2]==val){
+		if (arr[(start+end)/2] == val){
 				int i;
 				for (i=(start+end)/2;(i<end) && (arr[i+1]==val);++i){}
 				return i;
@@ -69,18 +67,17 @@ int find_last(const int * arr,const int start,\
 //------------------------------------------------------------------------------
 
 
-int count_simple(const int * arr,const int size,\
-					const int start,const int end)
+int count_simple(const int * arr,const int size,const int start,const int end)
 {
-		int first_pos=find_first(arr,0,size,start);
-		int last_pos=find_last(arr,0,size,end);
-		int total=0;
+		int first_pos = find_first(arr,0,size,start);
+		int last_pos = find_last(arr,0,size,end);
+		int total = 0;
 
 
-		if ((first_pos==-1) || (last_pos==-1))
+		if ((first_pos == -1) || (last_pos == -1))
 				return 0;
 
-		for (int i=first_pos;i<=last_pos;i++)
+		for (int i = first_pos;i<=last_pos;i++)
 				total+=is_simple(arr[i]);
 
 		return total;
@@ -91,19 +88,17 @@ int count_simple(const int * arr,const int size,\
 int main(int argc, char * argv[])
 {
 
-	int start,end;
+		int start,end;
 
-	if (((argc-1)%2!=0) || (argc<=1)){
-		return -1;
+		if (((argc-1)%2!=0) || (argc<=1)){
+				return -1;
 
-	}
-	for (int i=1;i<argc;i+=2){
-		start=atoi(argv[i]);
-		end=atoi(argv[i+1]);
-		std::cout<<count_simple(Data,Size,start,end)<<'\n';
+		}
+		for (int i=1;i<argc;i+=2){
+				start=atoi(argv[i]);
+				end=atoi(argv[i+1]);
+				std::cout<<count_simple(Data,Size,start,end)<<'\n';
+		}
 
-	}
-
-	return 0;
+		return 0;
 }
-
