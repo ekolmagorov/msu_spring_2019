21ff09b9ef78f9c9ea4a12bf22020c14607c111f
kolmX <47042803+kolmX@users.noreply.github.com>
2019-05-14 23:05:59 +0300

    fix code style

--- 05/05.cpp
+++ 05/05.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
 
+using namespace std;
+
+const static size_t N = 10;
+
+mutex m1;
+mutex m2;
+
+void ping()
+{
+    for(size_t i = 0; i < N; i++){
+        m1.lock();
+        cout << "ping" << endl;
+        m2.unlock();
+    }
+}
+
+void pong()
+{
+    for(size_t i = 0; i < N; i++){
+        m2.lock();
+        cout << "pong" << endl;
+        m1.unlock();
+    }
+}
+
+int main()
+{
+
+	m2.lock();
+    thread th1(ping);
+    thread th2(pong);
+
+    th1.join();
+    th2.join();
+
+    return 0;
+}
